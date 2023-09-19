76bc8b4b1aaf7c421fffa9584e30e29b3323e4e7
Denis <shafeev2002@mail.ru>
2023-09-19 17:30:19 +0500

    added new massages

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,18 +1,39 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 import math
+import time
 
 class BasePage():
-    def __init__(self, browser, url, timeout=5):
+    def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
 
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
-        except (NoSuchElementException):
+        except NoSuchElementException:
             return False
+        return True
+    
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
         return True
     
     def its_elements_equal(self, first_element, second_element):
--- pages/product_page.py
+++ pages/product_page.py
@@ -1,11 +1,23 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import time
 
 class ProductPage(BasePage):
     def add_product_to_basket(self):
         basket = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
+        #basket = WebDriverWait(self.browser, 4).until(EC.element_to_be_clickable((ProductPageLocators.BASKET_BUTTON)))
         basket.click()
         BasePage.solve_quiz_and_get_code(self)
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.NAME_IN_MASSANGE), \
+            "Success message is presented, but should not be"
+    
+    def should_succes_massage_dissapeard(self):
+        assert self.is_disappeared(*ProductPageLocators.NAME_IN_MASSANGE), \
+            "Success message in not desappeared, but should be"
 
     def should_be_correct_product_name(self):
         name_in_title = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT).text
